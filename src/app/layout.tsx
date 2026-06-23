b3e55f8abcea810137c15b242ecd3955b5312e59
Subhasis Roy <roysubhasis32@gmail.com>
2026-06-23 23:38:12 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const inter = Inter({ 
+  variable: '--font-inter', 
+  subsets: ['latin'] 
 })
 
 export const metadata: Metadata = {
@@ -46,7 +45,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} bg-background`}
+      className={`${inter.variable} bg-background`}
     >
       <body className="font-sans antialiased">
         {children}
